0f71ba8902bd79040d5b37d8ad29e50e998906d6
ammar mahmoud <93520305+Ammar-mahmoud@users.noreply.github.com>
2023-05-08 22:26:56 +0300

    Add files via upload

---
+++ BST.cpp
@@ -0,0 +1,218 @@
+//
+// Created by More on 06/05/2023.
+//
+
+#include "BST.h"
+
+void BST::insert_node() {
+    string name,depart;
+    float GPA;
+    int id;
+    cout << "id:"; cin >> id;
+    cin.ignore();
+    cout << "Name:"; getline(cin, name);
+    cout << "GPA:"; cin >>GPA;
+    cout << "Department:"; cin >> depart;
+    Node* new_node = new Node(name,depart,GPA,id);
+    if (root == NULL) {
+        root = new_node;
+        root->left = NULL;
+        root->right = NULL;
+    }
+    else {
+        Node* current = root;
+        bool  is_added = false;
+        while (!is_added)
+        {
+            if (id > current->id)
+            {
+                if (current->right == NULL)
+                {
+                    current->right = new_node;
+                    is_added = true;
+                }
+                else
+                    current = current->right;
+            }
+            else
+            {
+                if (current->left == NULL)
+                {
+                    current->left = new_node;
+                    is_added = true;
+                }
+                else
+                    current = current->left;
+            }
+        }
+    }
+    cout << "The student is added.\n";
+    cout << "-------------------------------\n";
+    length++;
+    if (sheet.count(depart))
+        sheet[depart]++;
+    else
+        sheet[depart] = 1;
+}
+
+void BST::search(int id) {
+    Node* current = root;
+    while (current != NULL)
+    {
+        if (id > current->id)
+        {
+            current = current->right;
+        }
+        else if (id < current->id)
+        {
+            current = current->left;
+        }
+        else
+        {
+            cout << "Student is found.\n";
+            cout <<'[' << current->id << ", " << current->name << ", " ;
+            cout << current->GPA << ", " << current->depart << "]\n";
+            return;
+        }
+    }
+    cout << "Student is not found.\n";
+    cout << "-------------------------------\n";
+}
+
+
+void BST::deleteNode() {
+    Node* curr = root;
+    Node* parent = nullptr;
+    int val; cout <<"id:"; cin >> val;
+    search(val);
+    // Find the node to be deleted and its parent
+    while (curr != nullptr && curr->id != val) {
+        parent = curr;
+        if (val < curr->id) {
+            curr = curr->left;
+        } else {
+            curr = curr->right;
+        }
+    }
+
+    // If the node wasn't found, return
+    if (curr == nullptr) {
+        return;
+    }
+
+    sheet[curr->depart]--;
+
+    // The node has no children
+    if (curr->left == nullptr && curr->right == nullptr) {
+        if (curr == root) {
+            root = nullptr;
+        } else if (curr == parent->left) {
+            parent->left = nullptr;
+        } else {
+            parent->right = nullptr;
+        }
+        delete curr;
+    }
+
+        // The node has one child
+    else if (curr->left == nullptr) {
+        if (curr == root) {
+            root = curr->right;
+        } else if (curr == parent->left) {
+            parent->left = curr->right;
+        } else {
+            parent->right = curr->right;
+        }
+        delete curr;
+    }
+    else if (curr->right == nullptr) {
+        if (curr == root) {
+            root = curr->left;
+        } else if (curr == parent->left) {
+            parent->left = curr->left;
+        } else {
+            parent->right = curr->left;
+        }
+        delete curr;
+    }
+
+        // The node has two children
+    else {
+        Node* successor = curr->right;
+        parent = curr;
+        while (successor->left != nullptr) {
+            parent = successor;
+            successor = successor->left;
+        }
+        curr->id = successor->id;
+        curr->name = successor->name;
+        curr->GPA = successor->GPA;
+        curr->depart = successor->depart;
+        if (parent->left == successor) {
+            parent->left = successor->right;
+        } else {
+            parent->right = successor->right;
+        }
+        delete successor;
+    }
+    cout << "Student is deleted.\n";
+    cout << "-------------------------------\n";
+    length--;
+
+}
+
+void BST::print() {
+    cout << "print: " << length << " students\n";
+    help_print(root);
+    cout << "Students per Departments: \n";
+    for(auto it : sheet)
+    {
+        cout << it.first << " " << it.second << " students" << endl;
+    }
+}
+
+void BST::help_print(Node *current) {
+    if (current != nullptr) {
+        help_print(current->left);
+        cout <<'[' << current->id << ", " << current->name << ", " ;
+        cout << current->GPA << ", " << current->depart << "]\n";
+        help_print(current->right);
+    }
+}
+
+void BST::work() {
+    cout << "Choose one of the following options:\n";
+    cout << "\t1. Add student \n";
+    cout << "\t2. Remove student\n";
+    cout << "\t3. Search student\n";
+    cout << "\t4. Print All (sorted by id)\n";
+    cout << "\t5. Return to main menu\n";
+    bool stop = false;
+    while (!stop)
+    {
+        int choose; cout << "Enter number of option:"; cin >> choose;
+        switch (choose)
+        {
+            case 1:
+                insert_node();
+                break;
+            case 2:
+                deleteNode();
+                break;
+            case 3:
+                int id; cout << "id:"; cin >>id;
+                search(id);
+                break;
+            case 4:
+                print();
+                break;
+            case 5:
+                stop = true;
+                break;
+        }
+        }
+}
+
+
+
+
--- main.cpp
+++ main.cpp
@@ -1,32 +1,6 @@
-#include <iostream>
-using namespace std;
-void display_menu() {
-    int choose=0;
-    while (choose!=5) {
-        cout<<"welcome in our program \nchoose your desired data structure:\n1)BST\n2) AVL\n3) Min Heap\n4) Max Heap\n5) Exit Program\n";
-        cin>>choose;
-        if(choose==5) {
-            cout<<"we are happy to see you :)";
-            break;
-        }
-        switch (choose) {
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            default:
-                cout<<"please enter a right choice\n";
-
-        }
-    }
-}
-
-
+#include "BST.h"
 int main() {
-    display_menu();
+    BST tree;
+    tree.work();
     return 0;
 }
