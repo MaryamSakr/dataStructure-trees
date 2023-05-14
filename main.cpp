644115280221c2201f82b4d533c512e46b3fe435
MaryamSakr <102592232+MaryamSakr@users.noreply.github.com>
2023-05-14 20:38:21 +0300

    main

--- main.cpp
+++ main.cpp
@@ -1,34 +1,136 @@
-#include <iostream>
-#include "AVL.cpp"
-using namespace std;
-void display_menu() {
-    int choose=0;
-    while (choose!=5) {
-        cout<<"welcome in our program \nchoose your desired data structure:\n1)BST\n2) AVL\n3) Min Heap\n4) Max Heap\n5) Exit Program\n";
-        cin>>choose;
-        if(choose==5) {
-            cout<<"we are happy to see you :)";
-            break;
-        }
+@@ -1,17 +1,126 @@
+#include "BST.h"
+#include "min_heap.h"
+#include "max_heap.h"
+#include "min_heap.h"
+
+void menu();
+void MinHeapMenu();
+void MaxHeapMenu();
+
+int main() {
+//    BST tree;
+//    tree.work();
+    menu();
+    return 0;
+}
+
+void menu(){
+    cout<<"1. BST\n2. AVL\n3. Min Heap\n4. Max Heap\n5. Exit Program\n";
+    int choose;
+    cout<<"1)MAx Heap\n2)Min Heap\n";
+    cin>>choose;
+    if(choose ==1){
+        MaxHeapMenu();
         switch (choose) {
             case 1:
                 break;
             case 2:
-                AVL a1;
                 break;
-//            case 3:
-//                break;
-//            case 4:
-//                break;
-//            default:
-//                cout<<"please enter a right choice\n";
-
+            case 3:
+                MinHeapMenu();
+                break;
+            case 4:
+                MaxHeapMenu();
+                break;
+            case 5:
+                exit(0);
+            default:
+                cout<<"Invalid choice\nPlease enter your choice again\n";
+                menu();
         }
-    }
-}
+        if(choose==2){
+            MinHeapMenu();
+        }
+
+        void MinHeapMenu(){
+            cout<<"1)Add student\n2)Print All (sorted by gpa)\n3)Return to main menu\n";
+            int ch;
+            cin>>ch;
+            if(ch==1){
+                addStudent();
+                cout<<"1)If you want to show Min Heap menu again\n2)If you want to close program\n3)Return to main menu\n";
+                int choose;
+                cin>>choose;
+                if(choose==1)
+                    MinHeapMenu();
+                if(choose==2)
+                    exit(0);
+                if(choose==3){
+                    menu();
+                }
+                else{
+                    cout<<"invalid choice.\nthe program will close.\n";
+                    exit(0);
+                }
+            }
+            if(ch==2){
+                printMinHeap(students);
+                cout<<"1)If you want to show Min Heap menu again\n2)If you want to close program\n3)Return to main menu\n";
+                int choose;
+                cin>>choose;
+                if(choose==1)
+                    MinHeapMenu();
+                if(choose==2)
+                    exit(0);
+                if(choose==3){
+                    menu();
+                }
+                else{
+                    cout<<"invalid choice.\nthe program will close.\n";
+                    exit(0);
+                }
+            }
+            if(ch==3){
+                menu();
+            }
+            else{
+                cout<<"invalid choice.\nthe program will close.\n";
+                exit(0);
+            }
+            return 0;
+        }
 
 
-int main() {
-    display_menu();
-    return 0;
-}
+        void MaxHeapMenu(){
+            cout<<"1)Add student\n2)Print All (sorted by gpa)\n3)Return to main menu\n";
+            int ch;
+            cin>>ch;
+            if(ch==1){
+                addStudent();
+                cout<<"1)If you want to show Max Heap menu again\n2)If you want to close program\n3)Return to main menu\n";
+                int choose;
+                cin>>choose;
+                if(choose==1)
+                    MaxHeapMenu();
+                if(choose==2)
+                    exit(0);
+                if(choose==3){
+                    menu();
+                }
+                else{
+                    cout<<"invalid choice.\nthe program will close.\n";
+                    exit(0);
+                }
+            }
+            if(ch==2){
+                printMaxHeap(students);
+                cout<<"1)If you want to show Max Heap menu again\n2)If you want to close program\n3)Return to main menu\n";
+                int choose;
+                cin>>choose;
+                if(choose==1)
+                    MaxHeapMenu();
+                if(choose==2)
+                    exit(0);
+                if(choose==3){
+                    menu();
+                }
+                else{
+                    cout<<"invalid chose.\nthe program will close.\n";
+                    exit(0);
+                }
+            }
+            if(ch==3){
+                menu();
+            }
+        }
